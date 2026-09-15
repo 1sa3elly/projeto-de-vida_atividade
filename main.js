540587af4a786c496fe1529ae5c1a8a42198d138
1sa3elly <isabelly.balbino@escola.pr.gov.br>
2026-09-15 14:24:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
+
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
 
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
   };
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-09-15T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = tempoFinal/1000;
+  return segundos;
+
 }
